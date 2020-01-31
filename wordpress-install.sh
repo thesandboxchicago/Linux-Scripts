f450f650f5e47f7826c57648f416b258b98c3386
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-01-31 01:51:14 -0500

    Update wordpress-install.sh

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -37,7 +37,7 @@ install-essentials
 ## Start Installation Of Wordpress
 function install-wordpress() {
     rm /var/www/html/index.nginx-debian.html
-    sed -i 's|index index.html index.htm index.nginx-debian.html;$|index.php;|' /etc/nginx/sites-available/default
+    sed -i 's|index index.html index.htm index.nginx-debian.html;$|index index.html index.php;|' /etc/nginx/sites-available/default
     cd /tmp
     wget https://wordpress.org/latest.tar.gz
     tar xf latest.tar.gz
